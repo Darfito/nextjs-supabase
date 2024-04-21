0b9cb61d2ce20c792672709aed3c088cdf498098
Darfito <darfito.danurdoro-2021@fst.unair.ac.id>
2024-04-21 17:12:59 +0700

    change to auth-helpers-nextjs

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
+// you must usingg the same packages for using client if you are using a middlleware, otherwise the session method didnt work
+import { createClientComponentClient} from "@supabase/auth-helpers-nextjs";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+export const supabase = createClientComponentClient()
 
-
-export const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
-);
